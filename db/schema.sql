af9fd71ef032c48435b288c25ccc81f75e428138
Jandrei Timoteo <jandrei_tim@yahoo.com>
2023-12-03 16:40:43 -0800

    refactored syntax in schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,23 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
-
 USE employees;
-
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT _PRIMARY KEY_,
-  name VARCHAR(_30_) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
-
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT _PRIMARY KEY_,
-    title VARCHAR(_30_) NOT NULL,
-    salary DECIMAL(_10_, _2_) NOT NULL,  _-- Added precision and scale for DECIMAL_
-    department_id INT NOT NULL,      _-- Corrected from department_id:_
-    _CONSTRAINT_ fk_department _FOREIGN KEY_ (department_id) _REFERENCES_ department(id) _ON DELETE CASCADE_
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,  -- Added precision and scale for DECIMAL
+    department_id INT NOT NULL,      -- Corrected from department_id:
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT _PRIMARY KEY_,
-    first_name VARCHAR(_30_) NOT NULL,
-    last_name VARCHAR(_30_) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    _CONSTRAINT_ fk_role _FOREIGN KEY_ (role_id) _REFERENCES_ role(id) _ON DELETE CASCADE_,
-    manager_id INT,                 _-- Corrected from manager_id:_
-    _CONSTRAINT_ fk_manager _FOREIGN KEY_ (manager_id) _REFERENCES_ employee(id) _ON DELETE_ SET NULL
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT,                 -- Corrected from manager_id:
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,24 @@
 USE employees;
-
 INSERT INTO
-    department (name)  _-- Corrected column name_
-
+    department (name)  -- Corrected column name
 VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal'),
     ('Sales');
-
+-- Assuming departments are inserted in the order above and get IDs 1, 2, 3, 4 respectively
 INSERT INTO
     role (title, salary, department_id)
 VALUES
-    ('Sales Lead', _100000_, _4_),
-    ('Salesperson', _80000_, _4_),
-    ('Lead Engineer', _150000_, _1_),
-    ('Software Engineer', _120000_, _1_),
-    ('Account Manager', _160000_, _2_),
-    ('Accountant', _125000_, _2_),
-    ('Legal Team Lead', _250000_, _3_),
-    ('Lawyer', _190000_, _3_);
-    
+    ('Sales Lead', 100000, 4),
+    ('Salesperson', 80000, 4),
+    ('Lead Engineer', 150000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Account Manager', 160000, 2),
+    ('Accountant', 125000, 2),
+    ('Legal Team Lead', 250000, 3),
+    ('Lawyer', 190000, 3);
+-- Assuming roles are inserted in the order above and get IDs 1, 2, 3, 4, etc., respectively
 INSERT INTO
     employee (
         first_name,
@@ -29,11 +27,11 @@ INSERT INTO
         manager_id
     )
 VALUES
-    ('Rachel', 'Green', _1_, null),
-    ('Gunther', 'Centralperk', _2_, _1_),
-    ('Ross', 'Geller', _3_, null),
-    ('Joey', 'Tribbiani', _4_, _3_),
-    ('Chandler', 'Bing', _5_, null),
-    ('Monica', 'Geller', _6_, _5_),
-    ('Phoebe', 'Buffay', _7_, null),
-    ('Mike', 'Hannigan', _8_, _7_);
+    ('Rachel', 'Green', 1, null),
+    ('Gunther', 'Centralperk', 2, 1),
+    ('Ross', 'Geller', 3, null),
+    ('Joey', 'Tribbiani', 4, 3),
+    ('Chandler', 'Bing', 5, null),
+    ('Monica', 'Geller', 6, 5),
+    ('Phoebe', 'Buffay', 7, null),
+    ('Mike', 'Hannigan', 8, 7);
